17b7f2e081874d447f847fea6a7054d2bb1daa97
Itggot-Victor-Ek <victor.ek@itggot.se>
2018-01-26 12:57:12 +0100

    Flyttade Authorization
    Authorization är en modell

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 class Main < Sinatra::Base
 
   enable :sessions
+  use Rack::Recaptcha, :public_key => '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', :private_key => '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+  helpers Rack::Recaptcha::Helpers
 
   get '/' do
     @authorization = Authorization.new
@@ -10,6 +12,21 @@ class Main < Sinatra::Base
   get '/home' do
 
     slim :home
+  end
+
+  get '/register' do
+
+    slim :register
+  end
+
+  post '/register' do
+    user = User.create(params['name'], params['username'], params['email'], params['password'], session)
+    redirect user.redirectURL
+  end
+
+  get '/user/:username' do
+
+    slim :user
   end
 
   get '/theo' do
