ce77f1502658a9c9b4944f150c1bb750e58971f8
Itggot-Victor-Ek <victor.ek@itggot.se>
2018-01-26 12:57:12 +0100

    Flyttade Authorization
    Authorization är en modell

--- models/authorization.rb
+++ models/authorization.rb
@@ -1,5 +1,5 @@
 class Authorization
-  attr_reader :data, :token
+  attr_accessor :data
   def initialize
     consumer_key = "VXiGD3igELfzYAQkVoJaKJXZewAa"
     consumer_secret = "N_51VglCPlf91Oj403HHkhsNUQYa"
@@ -20,8 +20,23 @@ class Authorization
     json_body = HTTParty.get(url, headers: api_auth_header).body
     @data = [JSON.parse(json_body)['LocationList']['StopLocation']]
 
-    url = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9021014003180000&date=2018-01-22&time=15%3A15&useTram=0&direction=9021014003220000&format=json"
+    url = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9021014003180000&date=2018-01-25&time=17%3A00&useTram=0&direction=9021014003220000&format=json"
     json_body = HTTParty.get(url, headers: api_auth_header).body
     @data << JSON.parse(json_body)['DepartureBoard']['Departure'][0]
+
+    url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.allstops?format=json"
+    json_body = HTTParty.get(url, headers: api_auth_header).body
+    data_allstops = JSON.parse(json_body)
+    p data_allstops
+
+
+    #db = SQLite3::Database.open('db/Västtrafik.sqlite')
+    #data_allstops.each_with_index do |hash,i|
+    #  name = hash['name']
+    #  id = i +1
+    #  db.execute('INSERT INTO all_stops VALUES (?,?)',id,name)
+    #end
+
   end
+
 end
