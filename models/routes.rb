9fa9b08b76ad2955fd02e70c039e42a23f2aee65
Itggot-Victor-Ek <victor.ek@itggot.se>
2018-02-01 23:25:47 +0100

    Hämta resor
    Fixa så man kan hämta resor från västtrafik och se dem på profilen, dock
    är de inte personliga än.

--- models/routes.rb
+++ models/routes.rb
@@ -1,14 +1,13 @@
 class Routes
-  def self.add_route_for_user(username, bearer_token, start_station, stop_station)
+  def self.add_route_for_user(username, bearer_token, start_station, stop_station, session)
     bearer_token = bearer_token
-    p bearer_token
     api_auth_header = {"Authorization" => "Bearer #{bearer_token}"}
+    station_name = stop_station
     start_station = URI::encode(start_station)
     stop_station = URI::encode(stop_station)
 
     url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=#{start_station}&format=json"
     json_body = HTTParty.get(url, headers: api_auth_header).body
-    p json_body
     start_station = JSON.parse(json_body)['LocationList']['StopLocation'][0]
     start_station_id = start_station["id"]
 
@@ -23,19 +22,34 @@ class Routes
 
     url = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=#{start_station_id}&date=2018-01-25&time=#{hour}%3A#{minutes}&direction=#{stop_station_id}&format=json"
     json_body = HTTParty.get(url, headers: api_auth_header).body
-    data = JSON.parse(json_body)['DepartureBoard']['Departure'][0]
-    depature_time = JSON.parse(json_body)['DepartureBoard']['Departure'][0]['time']
-    buss_name = JSON.parse(json_body)['DepartureBoard']['Departure'][0]['name']
 
+    if JSON.parse(json_body)['DepartureBoard'].to_s.include?('error')
+      session[:bad_route] = true
+      session[:bad_route_messege] = JSON.parse(json_body)['DepartureBoard']["errorText"]
+      return '/reseplanerare'
+    end
+    
     db = SQLite3::Database.open('db/Västtrafik.sqlite')
-
-    stop_id = db.execute('SELECT id FROM all_stops WHERE stop_name IS ?', [stop_station])[0]
-    db.execute('INSERT INTO departure (time, busid, stopid) VALUES(?, ?, ?)', [depature_time,buss_name,stop_id])
-
+    stop_id = db.execute('SELECT id FROM all_stops WHERE stop_name IS ?', [station_name])[0]
+    JSON.parse(json_body)['DepartureBoard']['Departure'].each do |hash|
+      db.execute('INSERT INTO departure (time, busid, stopid, input_date) VALUES(?, ?, ?, ?)', [hash["time"], hash["name"], stop_id, date])
+    end
+    session[:bad_route] = false
+    return '/user/test'
   end
 
   def self.get_route_for_user(username)
     db = SQLite3::Database.open('db/Västtrafik.sqlite')
-    return db.execute('SELECT * FROM depature')
+    elements = db.execute('SELECT * FROM departure')      #nytt namn på varibeln
+    elements.each do |array|
+      dates = array[4].split("-")
+      minutes = array[1][3..-1]
+      hours = array[1]
+      date = Time.new(dates[0], dates[1], dates[2], hours, minutes)
+      if Time.new() > date
+        db.execute('DELETE FROM departure WHERE id IS ?', [array[0]])
+      end
+    end
+    return db.execute('SELECT * FROM departure')
   end
 end
