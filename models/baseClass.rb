b4874a943ada000dcfd246340bdd51df00b33e69
Itggot-Victor-Ek <victor.ek@itggot.se>
2018-04-16 09:45:52 +0200

    create table update + beskrivining close #3
    den fungerade inte perfekt och gör säkert forfrande inte det, men den fungerar bättre

--- models/baseClass.rb
+++ models/baseClass.rb
@@ -21,7 +21,7 @@ class BaseClass
             end
 
             start_query = "CREATE TABLE [schema_name].#{@table_name}("
-            columns_query = self.join_columns(@columns)
+            columns_query = self.join_columns(@columns, use_row_id)
 
             final_query = start_query + columns_query + ")" + row_id_query
 
@@ -39,12 +39,19 @@ class BaseClass
 
     private
 
-    def self.join_columns(hash)    #parse_columns instead of join_columns?
+    #takes in a hash and joins the key and value to make a sqlite query
+    def self.join_columns(hash, use_row_id)
         final_string = ""
+        i = 0
         hash.each_pair do |key,value|
-            final_string += "#{key.to_s} #{value},"
+            if !use_row_id && i == 0
+                final_string += "#{key.to_s} #{value} PRIMARY KEY,"
+            else
+                final_string += "#{key.to_s} #{value},"
+            end
+            i+=1
         end
-        #quick fix to remoce the last comma
+        #quick fix to remove the last comma
         final_string = final_string[0..-2]
         return final_string
     end
