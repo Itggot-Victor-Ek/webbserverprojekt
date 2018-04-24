9deb320e5d390cb7ee87d094526ce942e1c22497
Itggot-Victor-Ek <victor.ek@itggot.se>
2018-04-24 22:43:54 +0200

    lite bättre namn? close #15
    Osäker på om 'apply' är rätt namn för att den då den inte bara anpassar
    dessa krav såsom 'Password' gör till viss del utan även kollar om de
    stämmer

--- models/baseClass.rb
+++ models/baseClass.rb
@@ -32,7 +32,7 @@ class BaseClass
         hash.each_pair do |key,value|
             if value.is_a? Array
                 columns_query += key.to_s + ','
-                result = self.check_requirements(value[1][:requirements], key.to_s, value.first, values)
+                result = self.apply_requirements(value[1][:requirements], key.to_s, value.first, values)
                 if result.is_a? Array
                     values = result[1]
                 elsif result
@@ -82,7 +82,7 @@ class BaseClass
         return final_string
     end
 
-    def self.check_requirements(requirements, column, value, values)
+    def self.apply_requirements(requirements, column, value, values)
         requirements_met = true
         requirements.each do |requirement|
             if requirement.is_a? Password
